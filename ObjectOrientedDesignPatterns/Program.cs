4bdd830f5ef47ee791524b3379f00369cc647d48
Yogananda Rao Locherla <locherla@live.com>
2021-07-04 10:00:56 +0530

    Add project files.

---
+++ ObjectOrientedDesignPatterns/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ObjectOrientedDesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
